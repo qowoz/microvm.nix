11d6e22ac5abe782f572b438d203aac98287b78b
zowoq <59103226+zowoq@users.noreply.github.com>
2024-12-12 20:52:07 +1000

    flake

--- flake.nix
+++ flake.nix
@@ -136,14 +136,14 @@
           ) {} (builtins.attrNames self.nixosConfigurations);
 
         # Takes too much memory in `nix flake show`
-        # checks = import ./checks { inherit self nixpkgs system; };
+        checks = import ./checks { inherit self nixpkgs system; };
 
         # hydraJobs are checks
-        hydraJobs = builtins.mapAttrs (_: check:
-          (nixpkgs.lib.recursiveUpdate check {
-            meta.timeout = 12 * 60 * 60;
-          })
-        ) (import ./checks { inherit self nixpkgs system; });
+        #hydraJobs = builtins.mapAttrs (_: check:
+        #  (nixpkgs.lib.recursiveUpdate check {
+        #    meta.timeout = 12 * 60 * 60;
+        #  })
+        #) (import ./checks { inherit self nixpkgs system; });
       }) // {
         lib = import ./lib { inherit (nixpkgs) lib; };
 
